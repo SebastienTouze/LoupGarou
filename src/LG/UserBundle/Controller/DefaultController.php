cd3e4ca05f28e815da36ae1a4d28888031d7e13d
sebtouze <sebtouze@gmx.fr>
2013-10-14 00:13:08 +0200

    Swiched variable name from french to english in default controler

--- src/LG/UserBundle/Controller/DefaultController.php
+++ src/LG/UserBundle/Controller/DefaultController.php
@@ -16,82 +16,82 @@ class DefaultController extends Controller
     {
         //Récupération de l'utilisateur en cours, des repositories et initialisation des valeurs
         $user = $this->container->get('security.context')->getToken()->getUser();
-        $em = $this->getDoctrine()->getEntityManager();
+        $em = $this->getDoctrine()->getManager();
         
-        $dbParametres = $em->getRepository('LGUserBundle:Parametres')->find(1);
+        $dbParameters = $em->getRepository('LGUserBundle:Parametres')->find(1);
         $users = $em->getRepository('LGUserBundle:User')->findAll();
         $votes = $em->getRepository('LGUserBundle:Vote')->findBy(array(), array('dateVote' => 'DESC'));
-        $votesUserAujourdhui = $em->getRepository('LGUserBundle:Vote')->findBy(array('votant' => $user->getId(), 'jour' => $dbParametres->getJour() ));
+        $votesUserToday = $em->getRepository('LGUserBundle:Vote')->findBy(array('votant' => $user->getId(), 'jour' => $dbParameters->getJour() ));
 
-        $votesEffectueesAujourdhui = array(1 => false, 2 => false, 3 => false);
+        $votesCastedToday = array(1 => false, 2 => false, 3 => false);
         
-        $joueursVivants = array();
-        $joueursMorts = array();
-        $listJoueursVote = array();
+        $alivePlayers = array();
+        $deadPlayers = array();
+        $listvotesPlayers = array();
+        $listvotesPlayersWW = array();
 
         //Changement de jour/nuit, et autres actions de fin de vote
-        $dbParametres = $this->changerJourNuit($dbParametres, $em);
-        
+        $dbParameters = $this->dayNightSwicher($dbParameters, $em);
         $em->flush();
 
-        foreach($votesUserAujourdhui as $vote)
-            {$votesEffectueesAujourdhui[$vote->getType()] = $vote->getVotePour();}
+        foreach($votesUserToday as $vote)
+            {$votesCastedToday[$vote->getType()] = $vote->getVotePour();}
         
         
         // Récupération des joueurs et génération des listes
-        foreach( $users as $joueur )
+        foreach( $users as $player )
         {
-            if($joueur->getVivant() == true)
+            if($player->getVivant() == true)
             {
-                $joueursVivants[$joueur->getId()] = $joueur; 
-                if ($joueur != $user)
+                $alivePlayers[$player->getId()] = $player; 
+                if ($player != $user)
                 {
-                    $listJoueursVote[$joueur->getId()] = $joueur->getUsername();
-                    if($joueur->getRole()->getId() != 2)
-                        {$listJoueursVoteLG[$joueur->getId()]= $joueur->getUsername();}
+                    $listvotesPlayers[$player->getId()] = $player->getUsername();
+                    if($player->getRole()->getId() != 2)
+                        {$listvotesPlayersWW[$player->getId()]= $player->getUsername();}
                 }
             }
             else
-                {$joueursMorts[$joueur->getDateMort()->format('d/m/Y').'-'.$joueur->getTypeMort()] = $joueur;}
+                {$deadPlayers[$player->getDateMort()->format('d/m/Y').'-'.$player->getTypeMort()] = $player;}
         }
         
         
         
         
         //Déterminer si l'on est le jour ou la nuit et gestion des paramètres
-        $jour = $dbParametres->getJour();
+        $dayNight = $dbParameters->getJour();
         
-        $parametre['jour'] = $jour == 1 ? "jour" : "nuit";
-        $parametre['heureNuit'] = $dbParametres->getheureNuit();
-        $parametre['heureJour'] = $dbParametres->getheureJour();
-        $maire = 'aucun';
-        if($testMaire = $em->getRepository('LGUserBundle:User')->findOneBy(array('maire' => true)) )
-            $maire = $testMaire;
+        $parameter['jour'] = $dayNight == 1 ? "jour" : "nuit";
+        $parameter['heureNuit'] = $dbParameters->getheureNuit();
+        $parameter['heureJour'] = $dbParameters->getheureJour();
+        $mayor = 'aucun';
+        if($testMayor = $em->getRepository('LGUserBundle:User')->findOneBy(array('maire' => true)) )
+            $mayor = $testMayor;
         
         
         
         
         //Formulaire pour le vote des vilageois
-        $voteContre = new Vote;
-        $formVoteVillage = $this->createFormBuilder($voteContre)
-                        ->add('votePour', 'choice', array('choices' => $listJoueursVote,
+        $voteAgainst = new Vote;
+        $formVoteVillage = $this->createFormBuilder($voteAgainst)
+                        ->add('votePour', 'choice', array('choices' => $listvotesPlayers,
             'multiple' => false, ))
                         ->add('type', 'hidden', array('data' => 1, ))
                         ->getForm();
         
         
         //Formulaire pour le vote des loups
-        $voteLG = new Vote;
-        $formVoteLoups = $this->createFormBuilder($voteLG)
-                        ->add('votePour', 'choice', array('choices' => $listJoueursVoteLG,
+        $voteWW = new Vote;
+        $formVoteWolfs = $this->createFormBuilder($voteWW)
+                        ->add('votePour', 'choice', array('choices' => $listvotesPlayersWW,
             'multiple' => false, ))
                         ->add('type', 'hidden', array('data' => '2'))
                         ->getForm();
         
         //Formulaire pour le vote d'élection du maire
         $voteM = new Vote;
-        $formVoteMaire = $this->createFormBuilder($voteM)
-                        ->add('votePour', 'choice', array('choices' => $joueursVivants,
+        $formMayorVote = $this->createFormBuilder($voteM)
+                        ->add('votePour', 'choice', array('choices' => $alivePlayers,
             'multiple' => false, ))
                         ->add('type', 'hidden', array('data' => '3'))
                         ->getForm();
@@ -100,21 +100,21 @@ class DefaultController extends Controller
         $formInfoHandler = new VoterHandler($formVoteVillage, $this->get('request'), $em, $user);
         if($formInfoHandler->process())
         {
-            return $this->render('LGUserBundle:Default:confirmation.html.twig', array('parametres' => $parametre));
+            return $this->render('LGUserBundle:Default:confirmation.html.twig', array('parameters' => $parameter));
         }
         else 
         {
-            $formInfoHandler = new VoterHandler($formVoteLoups, $this->get('request'), $em, $user);
+            $formInfoHandler = new VoterHandler($formVoteWolfs, $this->get('request'), $em, $user);
             if($formInfoHandler->process())
             {
-                return $this->render('LGUserBundle:Default:confirmation.html.twig', array('parametres' => $parametre));
+                return $this->render('LGUserBundle:Default:confirmation.html.twig', array('parameters' => $parameter));
             }
             else
             {
-                $formInfoHandler = new VoterHandler($formVoteMaire, $this->get('request'), $em, $user);
+                $formInfoHandler = new VoterHandler($formMayorVote, $this->get('request'), $em, $user);
                 if($formInfoHandler->process())
                 {
-                    return $this->render('LGUserBundle:Default:confirmation.html.twig', array('parametres' => $parametre));
+                    return $this->render('LGUserBundle:Default:confirmation.html.twig', array('parameters' => $parameter));
                 }
             }
         }
@@ -135,40 +135,40 @@ class DefaultController extends Controller
 
         
         //Récupération des votes par ordre anté-chronologique
-        $votesVillageois = array();
-        $votesMaire = array();
-        $votesLoups = array();
+        $villagersVotes = array();
+        $mayorVotes = array();
+        $wWVotes = array();
         
-        $jour = '';
+        $votesDay = '';
         
         foreach($votes as $vote)
         {
-            if($jour != $vote->getDateVote()->format('d/m/Y'))
+            if($votesDay != $vote->getDateVote()->format('d/m/Y'))
             {
-                $jour = $vote->getDateVote()->format('d/m/Y');
-                $votesVillageois[] = array('votant' => '*', 'pour' => $jour, );
-                if (array_key_exists($jour.'-2', $joueursMorts)) {$votesVillageois[] = array('votant' => 'x', 'pour' => $joueursMorts[$jour.'-2']);}
-                if (array_key_exists($jour.'-1', $joueursMorts)) {$votesVillageois[] = array('votant' => '+', 'pour' => $joueursMorts[$jour.'-1']);}
-                $votesLoups[] = array('votant' => '*','pour' => $jour, );
-                $votesMaire[] = array('votant' => '*','pour' => $jour, );
+                $votesDay = $vote->getDateVote()->format('d/m/Y');
+                $villagersVotes[] = array('votant' => 'date', 'pour' => $votesDay, );
+                if (array_key_exists($votesDay.'-2', $deadPlayers)) {$villagersVotes[] = array('votant' => 'wWDead', 'pour' => $deadPlayers[$votesDay.'-2']);}
+                if (array_key_exists($votesDay.'-1', $deadPlayers)) {$villagersVotes[] = array('votant' => 'burnedDead', 'pour' => $deadPlayers[$votesDay.'-1']);}
+                $wWVotes[] = array('votant' => 'date','pour' => $votesDay, );
+                $mayorVotes[] = array('votant' => 'date','pour' => $votesDay, );
             }
             switch ($vote->getType())
             {
             case 1:
             {
-                $votesVillageois[] = array('votant' => $vote->getVotant(),
+                $villagersVotes[] = array('votant' => $vote->getVotant(),
                                             'pour' => $vote->getVotePour(), );
                 break;
             }
             case 2:
             {
-                $votesLoups[] = array('votant' => $vote->getVotant(),
+                $wWVotes[] = array('votant' => $vote->getVotant(),
                                         'pour' => $vote->getVotePour(), );
                 break;
             }
             case 3:
             {
-                $votesMaire[] = array('votant' => $vote->getVotant(),
+                $mayorVotes[] = array('votant' => $vote->getVotant(),
                                         'pour' => $vote->getVotePour(), );
                 break;
             }
@@ -176,128 +176,128 @@ class DefaultController extends Controller
         }
         
         //Synthèse des votes en cours dans le village
-        $arraySyntheseVotesVillage = $em->getRepository('LGUserBundle:Vote')->findPersoneElue(1, $dbParametres->getJour());
-        $arraySyntheseVotesMaire = $em->getRepository('LGUserBundle:Vote')->findPersoneElue(3, $dbParametres->getJour());
+        $arraySynthesisVillageVotes = $em->getRepository('LGUserBundle:Vote')->findPersoneElue(1, $dbParameters->getJour());
+        $arraySynthesisMayorVotes = $em->getRepository('LGUserBundle:Vote')->findPersoneElue(3, $dbParameters->getJour());
         
         $userRepository = $em->getRepository('LGUserBundle:User');
-        foreach($arraySyntheseVotesMaire as $vote) 
+        foreach($arraySynthesisMayorVotes as $vote) 
         {
             $vote = $userRepository->find($vote['id']);
         }
-        foreach($arraySyntheseVotesVillage as $vote) 
+        foreach($arraySynthesisVillageVotes as $vote) 
         {
             $vote = $userRepository->find($vote['id']);
         }
         
         return $this->render('LGUserBundle:Default:index.html.twig', 
-                            array('user' => $user, 'joueursMorts' => $joueursMorts, 'joueursVivants' => $joueursVivants,
-                             'parametres' => $parametre, 'formJour' => $formVoteVillage->createView(),
-                             'formNuit' => $formVoteLoups->createView(), 'formMaire' => $formVoteMaire->createView(), 
-                             'votesVillageois' => $votesVillageois, 'votesMaire' => $votesMaire, 'votesLoups' => $votesLoups, 
-                             'votesEffectueesAujourdhui' => $votesEffectueesAujourdhui, 
-                             'formMessage' => $formMessage->createView(), 'messages' => $messages, 'maire' => $maire, 
-                             'votesParPersonneVillage' => $arraySyntheseVotesVillage, 
-                             'votesParPersonneMaire' => $arraySyntheseVotesMaire, ));
+                            array('user' => $user, 'deadPlayers' => $deadPlayers, 'alivePlayers' => $alivePlayers,
+                             'parameters' => $parameter, 'formJour' => $formVoteVillage->createView(),
+                             'formNuit' => $formVoteWolfs->createView(), 'formMaire' => $formMayorVote->createView(), 
+                             'villagersVotes' => $villagersVotes, 'mayorVotes' => $mayorVotes, 'wWVotes' => $wWVotes, 
+                             'votesCastedToday' => $votesCastedToday, 
+                             'formMessage' => $formMessage->createView(), 'messages' => $messages, 'maire' => $mayor, 
+                             'votesParPersonneVillage' => $arraySynthesisVillageVotes, 
+                             'votesParPersonneMaire' => $arraySynthesisMayorVotes, ));
     }
     
     
-    private function changerJourNuit($dbParametres, $em) {
+    private function DayNightSwicher($dbParameters, $em) {
         //Alternance Jour/Nuit
-        $heureCourante = intval(date("H"));
-        if( $dbParametres->getJour() == 0 && ($heureCourante > $dbParametres->getHeureJour() && $heureCourante < $dbParametres->getHeureNuit() ))
+        $currentTime = intval(date("H"));
+        if( $dbParameters->getJour() == 0 && ($currentTime > $dbParameters->getHeureJour() && $currentTime < $dbParameters->getHeureNuit() ))
         {
             
-            $arrayDevore = $em->getRepository('LGUserBundle:Vote')->findPersoneElue(3, $dbParametres->getJour());
+            $arrayDevoured = $em->getRepository('LGUserBundle:Vote')->findPersoneElue(3, $dbParameters->getJour());
             
-            $idDevore = $this->validerVote($arrayDevore);
+            $idDevoured = $this->validerVote($arrayDevoured);
             
-            if($idDevore)
+            if($idDevoured)
             {
-                $mort = $em->getRepository('LGUserBundle:User')->find($idDevore);
-                $mort->setVivant(false);
-                $mort->setMaire(false);
+                $dead = $em->getRepository('LGUserBundle:User')->find($idDevoured);
+                $dead->setVivant(false);
+                $dead->setMaire(false);
             
-                $em->persist($mort);
+                $em->persist($dead);
                 $em->flush();
             }
             else
-                {$dbParametres->setErreur(true);}
+                {$dbParameters->setErreur(true);}
             
             
             //Passage Nuit->Jour
-            $dbParametres->setJour(1);
+            $dbParameters->setJour(1);
             
         }
-        elseif($dbParametres->getJour() == 1 && ($heureCourante >= $dbParametres->getHeureNuit() || $heureCourante <= $dbParametres->getHeureJour() ))
+        elseif($dbParameters->getJour() == 1 && ($currentTime >= $dbParameters->getHeureNuit() || $currentTime <= $dbParameters->getHeureJour() ))
         {
             //Élection du maire et condamnation
-            $arrayCondamne = $em->getRepository('LGUserBundle:Vote')->findPersoneElue(1, $dbParametres->getJour());
-            $arrayMaire = $em->getRepository('LGUserBundle:Vote')->findPersoneElue(2, $dbParametres->getJour());
+            $arrayCondamned = $em->getRepository('LGUserBundle:Vote')->findPersoneElue(1, $dbParameters->getJour());
+            $arrayMayor = $em->getRepository('LGUserBundle:Vote')->findPersoneElue(2, $dbParameters->getJour());
             
-            $idCondamne = $this->validerVote($arrayCondamne);
-            $idMaire = $this->validerVote($arrayMaire, true);
+            $idCondamned = $this->validerVote($arrayCondamned);
+            $idMayor = $this->validerVote($arrayMayor, true);
             
-            if($idMaire)
+            if($idMayor)
             {
-                $maire = $em->getRepository('LGUserBundle:User')->find($maire);
-                $maire->setMaire(true);
-                $em->persist($maire);
+                $mayor = $em->getRepository('LGUserBundle:User')->find($idMayor);
+                $mayor->setMaire(true);
+                $em->persist($mayor);
             }
             else
-                {$dbParametres->setErreur(true);}
+                {$dbParameters->setErreur(true);}
             
-            if($idCondamne)
+            if($idCondamned)
             {
-                $mort = $em->getRepository('LGUserBundle:User')->find($idCondamne);
-                $mort->setVivant(false);
-                $mort->setMaire(false);
-                $em->persist($mort);
+                $dead = $em->getRepository('LGUserBundle:User')->find($idCondamned);
+                $dead->setVivant(false);
+                $dead->setMaire(false);
+                $em->persist($dead);
             }
             else
-                {$dbParametres->setErreur(true);}
+                {$dbParameters->setErreur(true);}
             
             $em->flush();
             
             //Passage Nuit->Jour
-            $dbParametres->setJour(0);
-            $dbParametres->jourSuivant();
+            $dbParameters->setJour(0);
+            $dbParameters->jourSuivant();
         }
         
-        return $dbParametres;
+        return $dbParameters;
     }
     
     
 
-    private function validerVote($arrayPersonnes, $voteFacultatif = false) {
+    private function validerVote($arrayPeople, $facultativeVote = false) {
     
         $oldNbVotes =  0;
-        $nbEgaux = 0;
+        $ndEquality = 0;
         
-        foreach($arrayPersonnes as $villageois)
+        foreach($arrayPeople as $villagers)
         {
-            if($villageois['nbVotes'] < $oldNbVotes)
+            if($villagers['nbVotes'] < $oldNbVotes)
                 break;
-            elseif($villageois['nbVotes'] >$oldNbVotes)
-                $oldNbVotes = $villageois['nbVotes'];
+            elseif($villagers['nbVotes'] >$oldNbVotes)
+                $oldNbVotes = $villagers['nbVotes'];
             else 
-                $nbEgaux++;
+                $ndEquality++;
         }
         
-        if($nbEgaux <1 && $voteFacultatif)
+        if($ndEquality <1 && !$facultativeVote)
         {
             throw new \Exception('Erreur : pas de votes ! Merci de signaler ce bug à Sébastien Touzé : sebastien.touze@ecl2012.ec-lyon.fr');
             return  false;
         }
-        elseif($nbEgaux >1)
+        elseif($ndEquality >1)
         {
             //TODO : envois d'un mail
             return false;
         }
-        elseif($nbEgaux == 1)
+        elseif($ndEquality == 1)
         {
-            $personne = $arrayPersonnes[$nbEgaux];
-            $idPersonne = $personne['id'];
-            return $idPersonne;
+            $person = $arrayPeople[$ndEquality];
+            $idPerson = $person['id'];
+            return $idPerson;
         }
     
     }
