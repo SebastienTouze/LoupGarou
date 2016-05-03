9ec0f67289a63e28a0da1afbc21fb8c92f8ce00f
Sébastien TOUZÉ <sebtouze@gmx.fr>
2016-05-03 16:17:59 +0200

    Added Twitter Bootstrap as a dependency in Composer. Adapt code to use it. Upgrate to Sf2.8.5

--- web/components/require-built.js
+++ web/components/require-built.js
@@ -2034,7 +2034,7 @@ var components = {
             "exports": "window.Modernizr"
         }
     },
-    "baseUrl": "components"
+    "baseUrl": "/components"
 };
 if (typeof require !== "undefined" && require.config) {
     require.config(components);
